c3a2c49e76843b79078ea56ab08d7ae6aab6074f
Oleksii Kryvtsov <nightmarelie@gmail.com>
2022-01-09 23:33:59 -0600

    init commit of RTC impl

---
+++ src/server.js
@@ -0,0 +1,35 @@
+import express from "express";
+import http from "http";
+import { Server } from "socket.io";
+import { v4 as uuidV4 } from "uuid";
+
+const app = express();
+const server = http.Server(app);
+const io = new Server(server);
+
+app.set("view engine", "ejs");
+app.set("views", "./src/views");
+app.use(express.static("public"));
+
+// console.log(__dirname);
+
+app.get("/", (req, res) => {
+  res.redirect(`/${uuidV4()}`);
+});
+
+app.get("/:room", (req, res) => {
+  res.render("room", { roomId: req.params.room });
+});
+
+io.on("connection", (socket) => {
+  socket.on("join-room", (roomId, userId) => {
+    socket.join(roomId);
+    socket.to(roomId).emit("user-connected", userId);
+
+    socket.on("disconnect", () => {
+      socket.to(roomId).emit("user-disconnected", userId);
+    });
+  });
+});
+
+server.listen("3000");
